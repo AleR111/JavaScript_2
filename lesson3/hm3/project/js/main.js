c1ee45f51272604c04d752a7276bfe7f1ca32e58
Al <alexseier12345@gmail..com>
2021-04-27 22:10:06 +0300

    create hm3

---
+++ lesson3/hm3/project/js/main.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// Переделать в ДЗ не использовать fetch а Promise
+let getRequest = (url, cb) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+                console.log('Error');
+            } else {
+                cb(xhr.responseText);
+            }
+        }
+    };
+    xhr.send();
+};
+
+// –--------------------------------
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this._goods = [];
+        this._allProducts = [];
+
+        // this._fetchGoods();
+        // this._render();
+
+        this._getProducts()
+            .then((data) => {
+                this._goods = data;
+                this._render();
+            });
+    }
+
+    // _fetchGoods() {
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this._goods = JSON.parse(data);
+    //         console.log(this._goods);
+    //         this._render();
+    //     });
+    // }
+
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then((response) => response.json())
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (const good of this._goods) {
+            const productObject = new ProductItem(good);
+            // console.log(productObject);
+            this._allProducts.push(productObject);
+            block.insertAdjacentHTML('afterbegin', productObject.render());
+        }
+    }
+
+    getTotalPriceOfProduct() {
+       return this._goods.reduce((total, elem) =>
+           total +=  elem.price
+        , 0)
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                      <img src="${this.img}" alt="Some img">
+                      <div class="desc">
+                          <h3 class="product-item__header">${this.title}</h3>
+                          <p class="product-item__text">${this.price} \u20bd</p>
+                          <button class="buy-btn">Купить</button>
+                      </div>
+                  </div>`;
+    }
+}
+const product = new ProductList();
+console.log(product.getTotalPriceOfProduct());
