c2595e2be14da35530447d37be8705fa6e4e105b
Al <alexseier12345@gmail..com>
2021-05-06 00:41:52 +0300

    done search

--- lesson5/hm5/project/js/main.js
+++ lesson5/hm5/project/js/main.js
@@ -1,37 +1,42 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    catalogUrl: '/catalogData.json',
-    products: [],
-    imgCatalog: 'https://placehold.it/200x150'
-  },
-  methods: {
-    getJson(url){
-      return fetch(url)
-        .then(result => result.json())
-        .catch(error => {
-          console.log(error);
-        })
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'https://via.placeholder.com/200x150',
+        searchLine: '',
+        productsSearched: []
     },
-    addProduct(product){
-      console.log(product.id_product);
-    }
-  },
-  beforeCreate() {},
-  created() {
-    this.getJson(`${API + this.catalogUrl}`)
-      .then(data => {
-        for(let el of data){
-          this.products.push(el);
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+        },
+        filterGoods() {
+            this.productsSearched = [];
+            this.products.forEach(elem => {
+                if(elem.product_name.toLocaleLowerCase().includes(this.searchLine.toLocaleLowerCase())) this.productsSearched.push(elem)
+            });
+
+            console.log(this.productsSearched)
         }
-      });
-  },
-  beforeMount() {},
-  mounted(){},
-  beforeUpdate() {},
-  updated() {},
-  beforeDestroy() {},
-  destroyed() {},
+    },
+
+    created() {
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+    },
+
 });
