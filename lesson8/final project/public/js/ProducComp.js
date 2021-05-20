c12613b0cdc74f91f276a37cb0eefae1ad384872
Al <alexseier12345@gmail..com>
2021-05-21 00:47:42 +0300

    created html form catalog items in app

--- lesson8/final project/public/js/ProducComp.js
+++ lesson8/final project/public/js/ProducComp.js
@@ -23,7 +23,7 @@ Vue.component('products', {
             });
     },
     template: `
-        <div class="products">
+        <div class="product__cards">
             <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
         </div>
     `
@@ -37,13 +37,24 @@ Vue.component('product', {
     },
 
     template: `
-    <div class="product-item">
-                <img :src="img" alt="Some img">
-                <div class="desc">
-                    <h3>{{product.product_name}}</h3>
-                    <p>{{product.price}}₽</p>
-                    <button class="buy-btn" @click="cartAPI.addProduct(product)">Купить</button>
-                </div>
+<!--    <div class="product-item">-->
+<!--                <img :src="img" alt="Some img">-->
+<!--                <div class="desc">-->
+<!--                    <h3>{{product.product_name}}</h3>-->
+<!--                    <p>{{product.price}}₽</p>-->
+<!--                    <button class="buy-btn" @click="cartAPI.addProduct(product)">Купить</button>-->
+<!--                </div>-->
+<!--            </div>-->
+            
+            
+           <div class="product__card">
+                <a href="#"><img class="product__img" :src="product.img_src" alt="item"></a>
+                <a class="product__cart" href="#"><img src="img/product-cart.svg" alt="cart"> Add to Cart</a>
+                <div class="product__text">
+                    <a class="product__name" href="#">{{product.product_name}}</a>
+                    <p class="text">{{product.description}}</p>
+                    <p class="product__price">\${{product.price}}</p>
+               </div>
             </div>
     `
 });
